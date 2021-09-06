978753c4a9e16aa7975fea486a815083b6ab8eb9
ceconcarlsen <ceconcarlsen@gmail.com>
2021-09-06 16:04:42 -0300

    static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,36 @@
+import BarChart from "Components/BarChart";
+import DonutChart from "Components/DonutChart";
+
 import DataTable from "Components/DataTable";
+
 import Footer from "Components/Footer";
 import Navbar from "Components/Navbar";
 
 function App() {
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <div className="container">
-        <DataTable/>
+        <h1 className="text-primary py-3">DashBoard de Vendas</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso %</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso %</h5>
+            <DonutChart />
+          </div>
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary">Todas vendas</h2>
+        </div>
+
+        <DataTable />
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 }
